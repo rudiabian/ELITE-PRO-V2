f3b34413eae7cb8a29e017e44b960a0a0e55ea04
rudiabian <rudi.rahmad7913@gmail.com>
2025-08-18 05:11:56 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,23 +3,23 @@ const chalk = require('chalk')
 
 //BOT SETTINGS//
 global.xprefix = '.'
-global.ownername = 'ElitePro'
-global.botname = 'ELITE-PRO-V2'
+global.ownername = 'bang ARR'
+global.botname = 'Dark_Side-Elite'
 global.ytname = "YT: EliteProTechs"
 global.socialm = "GitHub: EliteProTech"
-global.location = "Port Harcourt, Nigeria"
-global.ownernumber = '2348109263390'
+global.location = "Port alamsemesta"
+global.ownernumber = '60142808841'
 
 //BOT SETTINGS 2//
-global.premium = ["2348109263390"] //Premium 
+global.premium = ["60142808841"] //Premium 
 global.websitex = "https://youtube.com/@eliteprotechs"
-global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
+global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd"
 global.themeemoji = '👨🏼‍💻'
-global.wm = "ELITE-PRO-V2"
+global.wm = "ELITE-Dark_Side-PRO-V2"
 global.botscript = 'https://github.com/EliteProTech/Elite-Pro-V2'
 global.packname = "Sticker By"
 global.author = "ELITE-PRO-V2\n\n+2348109263390"
-global.creator = "2348109263390@s.whatsapp.net"
+global.creator = "60142808841@s.whatsapp.net"
 global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg'
 
 //DEFAULT SETTINGS//
